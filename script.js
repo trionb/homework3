f3c02f5a6b752515fcf791ceae5dbb03226eb411
trionb <trionbailey@gmail.com>
2020-02-06 22:52:02 -0500

    prompt update

---
+++ script.js
@@ -0,0 +1,60 @@
+// // // Assignment Code
+//  var generateBtn = document.querySelector("#generate");
+ 
+//  values="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~"
+ 
+ 
+// // // Write password to the #password input
+//    function writePassword() {
+//    var password = generatePassword();
+//    var passwordText = document.querySelector("#password");
+   
+    
+//    passwordText.value = password;
+
+//     for(var i=o; 1 <= generateBtn; i++) {
+//     password=password + values.charAt(Math.floor(Math.random()* Math.floor(values.length-1)));
+
+//    }
+
+
+//   // Add event listener to generate button
+   //generateBtn.addEventListener("click", writePassword);
+
+ 
+
+
+    // document.getElementById("generate").addEventListener("click", function(){
+    //   prompt("How many characters would you like your password to contain");
+
+  
+
+
+       function generate() {
+      prompt("How many characters would you like your password to contain");
+          for(var i =0; i =>128; i++){
+            console.log(i);
+          }
+            
+      confirm("Do you want special characters?");
+
+      confirm("Do you want numeric Characters");
+
+      confirm("Do you want lowercase Characters");
+
+      confirm("Do you want uppercase Characters");
+   }
+
+
+   
+
+    
+
+// function geRandomLower(){
+//   return String.fromCharCode(97);
+// }
+// console.log(Math.floor(Math.random() * 26) + 97)
+
+
+// var characterLength
+// var specialCharacters 
